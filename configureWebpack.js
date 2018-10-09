f83054190d92efc85e2228a4d55be67a37dc3714
Anselm McClain <atm@xh.io>
2018-10-09 14:38:58 -0700

    Remove `url:false` css-loader config - we need to process url includes

--- configureWebpack.js
+++ configureWebpack.js
@@ -247,8 +247,7 @@ function configureWebpack(env) {
                                     loader: 'css-loader',
                                     options: {
                                         importLoaders: 2, // Indicate how many prior loaders (postCssLoader/sassLoader) to also run on @imported resources.
-                                        sourceMap: true,
-                                        url: false
+                                        sourceMap: true
                                     }
                                 },
 
